5ca21c20d8f9e20b7817ff85774b7af97d604090
FranVlahovic <franvlahovic@proton.me>
2025-05-17 01:50:13 +0200

    feat: Update App JSX, CSS, StartScreen JSX. Updated styling to make game responsive.

--- src/App.jsx
+++ src/App.jsx
@@ -336,6 +336,7 @@ export default function App() {
                 avgScore={avgScore}
                 playAgain={playAgain}
             />}
+            
         </>
     );
 }
--- src/screens/StartScreen/StartScreen.jsx
+++ src/screens/StartScreen/StartScreen.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react";
+
 export default function StartScreen({ handleScreenSwitch }) {
+    const [isMobile, setIsMobile] = useState(window.innerWidth<=768)
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= 768);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    if(isMobile){
+        return (
+            <div className="startscreen">
+                <div className="startscreen-heading">
+                    <h1>Announcement</h1>
+                    <p>This droid prefers desktops! Try again on a larger screen for full functionality.</p>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className="startscreen" onClick={()=>handleScreenSwitch('disclaimer')}>
